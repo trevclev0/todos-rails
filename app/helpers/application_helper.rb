15a4dafe33d8b05621bdff12e91bbfa6e2ebc4dc
Trevor Durán <trevor.m.duran@gmail.com>
2026-02-25 19:46:27 -0600

    feat: add helper logic for notifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def bootstrap_class_for_flash(flash_type)
+    case flash_type.to_s
+    when "notice" then "success"
+    when "alert"  then "danger"
+    else "info"
+    end
+  end
 end
