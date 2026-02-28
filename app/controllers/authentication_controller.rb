543c78a2e21da58c85452002ffe617734989bc12
Trevor Durán <trevor.m.duran@gmail.com>
2026-02-28 15:40:27 -0600

    feat: require user to login after registration

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -7,8 +7,7 @@ class AuthenticationController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path, notice: "Welcome aboard!"
+      redirect_to :login, notice: "Registration successful. Please login"
     else
       render :register, status: :unprocessable_entity
     end
