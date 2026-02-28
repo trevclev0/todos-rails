42e9758ac7c0a2fb68279f71a64814db017f2800
Trevor Durán <trevor.m.duran@gmail.com>
2026-02-28 12:23:09 -0600

    feat: add support for error Rails flash type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def bootstrap_class_for_flash(flash_type)
     case flash_type.to_s
     when "notice" then "success"
-    when "alert"  then "danger"
+    when "alert", "error"  then "danger"
     else "info"
     end
   end
