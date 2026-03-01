a8074f076bbe3ab3ff913ce5dc7bf0a5a0e943c0
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-01 09:27:10 -0600

    feat: add migration to add user_id to todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_22_131442) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_01_133200) do
   create_table "todos", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.boolean "completed", default: false, null: false
+    t.datetime "created_at", null: false
+    t.string "description"
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username", null: false
-    t.string "password_digest", null: false
     t.datetime "created_at", null: false
+    t.string "password_digest", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 end
