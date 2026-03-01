47f59d8dfaeed3658a1bd036c20a983a03dd0e6f
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-01 07:12:40 -0600

    feat: add require_login logic to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    redirect_to login_path, alert: "You must be logged in." unless current_user
+  end
+
+  helper_method :current_user
 end
