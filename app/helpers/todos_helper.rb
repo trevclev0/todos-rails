5e9684be0adeabfb797c6d2b448b52dc37194086
Trevor Durán <trevor.m.duran@gmail.com>
2026-02-28 18:29:29 -0600

    feat: implement form partial

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -2,4 +2,8 @@ module TodosHelper
   def format_todo_status(todo)
     todo.completed? ? "☑ Completed" : "☐ Incomplete"
   end
+
+  def todo_form_title(todo)
+    todo.new_record? ? "New Todo" : "Edit Todo"
+  end
 end
